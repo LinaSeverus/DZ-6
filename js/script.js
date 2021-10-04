0681e6742b7400d3e7bc71e68fb80dba7acc740c
LinaSeverus <severus@ukr.net>
2021-10-04 20:46:36 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Task 1
+
+// Boolean(Number(’10’)) + 1;  Результат = 2       /Number/
+//  `sub ` + Number(false);    Результат = sub 0  /string/ 
+//  16 * `      91    `;       Результат = 1456   /Number/
+//  true - 70;                 Результат = -69    /Number/
+// Number(1 + String(1)) + 1;  Результат = 12     /Number/
+
+// Task 2
+
+let hour = prompt('Введите количество часов');
+
+let seconds = (+hour * 3600) / 1;
+
+alert(`В ${hour} ч будет ${seconds} секунд`);
+
+// Task 3
+
+var num = 1;
+
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num++;
+num--;
+
+alert(num);
+
